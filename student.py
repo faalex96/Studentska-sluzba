107571e2e7dd79bff7150380508c3ad31ac3f317
Aleksandar Fa <fa.e277@uns.ac.rs>
2020-12-01 20:21:28 +0100

    Added concept of passing and forwarding the subject. Printing passed subjects, calculating students average grade.

--- student.py
+++ student.py
@@ -7,6 +7,7 @@ class Person:
 
     @property
     def full_name(self):
+        """Returns full name of a person."""
         return "{} {}".format(self.first_name, self.last_name)
 
 
@@ -35,10 +36,12 @@ class Subject:
         self.ESPB = ESPB
         self.assignements = []
         self.grade = 5
+        self.forwared = False
 
-    def add_assignement(self, assign):
+    def add_assignements(self, *assign):
         """Adds assignement into subject assignements list"""
-        self.assignements.append(assign)
+        for a in assign:
+            self.assignements.append(a)
 
     def calculate_grade(self):
         """Calculates grade based on assignment points"""
@@ -46,7 +49,7 @@ class Subject:
         for assign in self.assignements:
             total_score += assign.points
 
-        if total_score > 50 and total_score < 60:
+        if total_score >= 50 and total_score < 60:
             self.grade = 6
         elif total_score >= 60 and total_score < 70:
             self.grade = 7
@@ -58,6 +61,16 @@ class Subject:
             self.grade = 10
 
         return total_score
+    
+    @property
+    def passed(self):
+        if self.calculate_grade() >= 50:
+            return True
+        return False
+
+    def forward_subject(self):
+        """ Change forward subject to True. """
+        self.forwared = True
 
     def find_assignement(self, assign_title):
         """Return assignement under specified title"""
@@ -74,10 +87,13 @@ class Subject:
         return "#{0} \"{1:^20s}\", ESPB:{2:2s} GRADE:{3}".format(self.code, self.title, str(self.ESPB), self.grade)
 
 mehanika = Subject("Mehanika","101",9)
-mehanika.add_assignement(a)
-mehanika.add_assignement(a2)
-mehanika.add_assignement(a3)
+mehanika.add_assignements(a,a2,a3)
 mehanika.print_assignements()
+
+racunarstvo = Subject("Racunarstvo", "101", 12)
+racunarstvo.add_assignements(Assignment("OOP", datetime(2020,12,2)))
+racunarstvo.add_assignements(Assignment("Concurrency", datetime(2020, 12,10)))
+
 
 class Student(Person):
     def __init__(self, first_name, last_name, email, year, course, funding):
@@ -87,11 +103,13 @@ class Student(Person):
         self.course = course
         self.funding = funding
         self.subjects = []
+        self.passed_subs = []               # Necessary for eliminating multiple forwarding of the same subject
         self.ESPB = 0
     
-    def add_subject(self, subject):
+    def add_subject(self, *subs):
         """ Adds subject to students subjects list"""
-        self.subjects.append(subject)
+        for sub in subs:
+            self.subjects.append(sub)
 
     def find_subject(self, subject_title):
         """ Returns subject under specified title"""
@@ -99,27 +117,73 @@ class Student(Person):
             if sub.title == subject_title:
                 return sub
 
+    def print_all_subjects(self):
+        """Prints all subjects"""
+        for sub in self.subjects:
+            print(sub)
+
     def change_assignement_points(self, subject_title, assignement_title, points):
         """Find subject, and its's assignment and change it's points"""
         subject = self.find_subject(subject_title)
         assign = subject.find_assignement(assignement_title)
         assign.change_points(points)
         subject.calculate_grade()
+    
+    def forward_subject(self, subject_title):
+        """ If subject is passed and forwarded it will show up in passed subject print
+        with it's code, title, ESPB point and grade."""
+        subject = self.find_subject(subject_title)
+        subject.forward_subject()
+        if subject.passed and subject.forwared:
+            if subject not in self.passed_subs:             # Make sure you don't forward same subject twice
+                self.ESPB += subject.ESPB
+                self.passed_subs.append(subject)
 
-    def print_subjects(self):
-        """Prints all subjects"""
+    def print_passed_subjects(self):
+        """ Prints all passed and forwarded subjects"""
         for sub in self.subjects:
-            print(sub)
+            if sub.passed and sub.forwared:
+                print(sub)
+        #for sub in self.passed_subs:
+        #    print(sub)
+
+    def enroll_new_year(self, tresh_year, tresh_budget):
+        """There are two ESPB tresholds: year and budget treshold. 
+        If student passed year treshold, he can be enrolled to next year.
+        If student passed budget treshold, he can be enrolled to next year on budget."""
+        if self.ESPB >= tresh_year:
+            self.year += 1
+            if self.ESPB >= tresh_budget:
+                self.funding = "budget"
+            else:
+                self.funding = "personal"
+        else:
+            print("Can't enroll student. Not enough ESPB points.")
+
+    @property
+    def average_grade(self):
+        avg = 0
+        for sub in self.passed_subs:
+            avg += sub.grade
+        return avg/len(self.passed_subs)
 
     def __str__(self):
-        return "{0}, year: {1}, course: {2}, funding: {3}.".format(self.full_name, self.year, self.course, self.funding)
+        return "{0}, year: {1}. course: {2}, ESPB:{3}, Avg:{4:.2f}, funding: {5}.".format(self.full_name, self.year, self.course, self.ESPB, self.average_grade,self.funding)
         
-Sale = Student("Aleksandar","Fa","aleksandarfa@dev.gmail", 1, "Biomedical engineering","budgete")
+Sale = Student("Aleksandar","Fa","aleksandarfa@dev.gmail", 1, "Biomedical engineering","budget")
 Sale.add_subject(mehanika)
+Sale.add_subject(racunarstvo)
 
 Sale.change_assignement_points("Mehanika", "Upravljanje toka programa", 30)
 Sale.change_assignement_points("Mehanika", "For i While petlja", 30)
 Sale.change_assignement_points("Mehanika", "Funkcije", 30)
+Sale.forward_subject("Mehanika")
 
-Sale.print_subjects()
-print()
+#Sale.print_all_subjects()
+Sale.change_assignement_points("Racunarstvo", "OOP", 30)
+Sale.change_assignement_points("Racunarstvo","Concurrency", 21)
+Sale.forward_subject("Racunarstvo")
+Sale.forward_subject("Racunarstvo")
+Sale.print_passed_subjects()
+print(Sale)
+Sale.enroll_new_year(36, 48)
